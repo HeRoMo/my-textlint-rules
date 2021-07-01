6a65af33da015c95dc54b4469ddf9fb5cb127c76
HeRoMo <hero@asterisk-works.jp>
2021-07-01 21:35:38 +0900

    upgrade textlint-filter-rule-allowlist

--- scripts/convert-yml-to-json.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const yaml = require('yaml');
-
-const yamlData = fs.readFileSync('./.textlintrc.yml','utf8');
-const config = yaml.parse(yamlData);
-delete config.filters.allowlist
-fs.writeFileSync('./.textlintrc.json', JSON.stringify(config));
